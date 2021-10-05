c098bb25fc4c5ff1d2a78d0776603078b8635161
Pavels Garklavs <pavelsgarklavs@gmail.com>
2021-10-05 23:56:42 +0300

    Initial Commit

---
+++ iOsCookieClicker/iOsCookieClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOsCookieClickerApp.swift
+//  iOsCookieClicker
+//
+//  Created by Pāvels Garklāvs on 05/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct iOsCookieClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
